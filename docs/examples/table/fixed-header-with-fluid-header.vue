68a8477e96d8ac58509bdb285d221b6d78339828
mylixiangxiang <101060605+mylixiangxiang@users.noreply.github.com>
2024-11-23 20:47:17 +0800

    Update fixed-header-with-fluid-header.vue

--- docs/examples/table/fixed-header-with-fluid-header.vue
+++ docs/examples/table/fixed-header-with-fluid-header.vue
@@ -1,11 +1,7 @@
 <template>
   <el-table :data="tableData" style="width: 100%" max-height="250">
     <el-table-column fixed prop="date" label="Date" width="150" />
-    <el-table-column prop="name" label="Name" width="120" />
-    <el-table-column prop="state" label="State" width="120" />
-    <el-table-column prop="city" label="City" width="120" />
-    <el-table-column prop="address" label="Address" width="600" />
-    <el-table-column prop="zip" label="Zip" width="120" />
+
     <el-table-column fixed="right" label="Operations" min-width="120">
       <template #default="scope">
         <el-button
